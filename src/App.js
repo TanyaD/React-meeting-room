c849fb643a736bc839b18ec17cd888a48651ffd2
TanyaD <tat.dbk@gmail.com>
2019-08-12 18:11:39 -0700

    initiate project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+import Home from './Home'
+
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state={
+      user:null
+    }
+  }
+
+  render() {
+    return (
+      <Home user={this.state.user} />
+    );
+  }
+}
+
+export default App;
