d5ecf1b8e182ad5a3aff6446c79df3c4c11b3063
Tanya Dubinchyk <tat.dbk@gmail.com>
2019-08-13 17:53:45 -0700

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,13 +4,7 @@ import 'firebase/auth';
 
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "react-meetings-4fe0e.firebaseapp.com",
-    databaseURL: "https://react-meetings-4fe0e.firebaseio.com",
-    projectId: "react-meetings-4fe0e",
-    storageBucket: "",
-    messagingSenderId: "1042671744753",
-    appId: "1:1042671744753:web:7f4af30d6e504433"
+
   };
 
   firebase.initializeApp(firebaseConfig);
