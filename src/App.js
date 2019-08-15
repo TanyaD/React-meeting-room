75bfdc2b639bb417800441bce45621dc77e1c64e
Tanya Dubinchyk <tat.dbk@gmail.com>
2019-08-14 17:52:26 -0700

    Add Login/Logout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Router} from '@reach/router';
+import {Router, navigate} from '@reach/router';
 import firebase from './Firebase'
 
 import 'bootstrap/dist/css/bootstrap.css';
@@ -16,33 +16,77 @@ class App extends Component {
 
   constructor(){
     super();
-    this.state={
-      user:null
-    }
+    this.state = {
+      user: null,
+      displayName: null,
+      userID: null
+    };
   }
 
   componentDidMount(){
-    const ref = firebase.database().ref('user')
-    ref.on('value', snapshot =>{
-      let FBUser = snapshot.val();
-      this.setState({user: FBUser})
-    })
+    firebase.auth().onAuthStateChanged(FBUser => {
+      if(FBUser){
+        this.setState({
+          user: FBUser,
+          displayName: FBUser.displayName,
+          userID: FBUser.uid
+        });
+      }
+    });
   }
+
+  registerUser = userName => {
+    firebase.auth().onAuthStateChanged(FBUser => {
+      FBUser.updateProfile({
+        displayName: userName
+      }).then(()=>{
+        this.setState({
+          user: FBUser,
+          displayName: FBUser.displayName,
+          userID: FBUser.uid
+        });
+        navigate('/meetings');
+      });
+    });
+  };
+
+  logOutUser = e => {
+    e.preventDefault();
+    this.setState({
+        user:null,
+        displayName:null,
+        userID: null
+    })
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+      navigate('/login')
+    });
+  };
 
   render() {
     return (
       <div>
-        <Navigation user={this.state.user} />
-        {this.state.user && <Welcome user={this.state.user} />}
+        <Navigation 
+          user={this.state.user}
+          logOutUser={this.logOutUser}
+        />
+        {this.state.user && (
+          <Welcome 
+            userName={this.state.displayName}
+            logOutUser={this.logOutUser} 
+          />
+        )}
         <Router>
           <Home path="/" user={this.state.user} />
           <Login path="/login" />
           <Meetings path="/meetings" />
-          <Register path="/register" />
+          <Register path="/register"  registerUser={this.registerUser}/>
         </Router>
       </div>
-    );
+      );
+    }
   }
-}
 
 export default App;
