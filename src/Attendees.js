f661c6dabd3493d6cd300a966fb946b5fcf7bbf0
Tanya Dubinchyk <tat.dbk@gmail.com>
2019-08-25 22:13:39 -0700

    Create Attendees.js

---
+++ src/Attendees.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from '@reach/router';
+
+class Attendee extends Component{
+    render(){
+        return(
+          <div>Attendees</div>
+            )
+    }
+}
+
+export default Attendee;
