f11726db9a7535197c40d7f5684bf553efda7c7c
Tanya Dubinchyk <tat.dbk@gmail.com>
2019-08-26 18:52:43 -0700

    Update Attendees.js

--- src/Attendees.js
+++ src/Attendees.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import { Link } from '@reach/router';
+import firebase from './Firebase'
 
 class Attendee extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            displayAttendees:[]
+        }
+    }
+
+    componentDidMount(){
+        const ref = firebase.database().ref(`meetings/${this.props.userID}/${this.props.meetingID}/attendees`)
+    }
+
     render(){
         return(
-          <div>Attendees</div>
-            )
+            <div className="container mt-4">
+              <div className="row justify-content-center">
+                <div className="col-md-8">
+                  <h1 className="font-weight-light text-center">
+                    Attendees
+                  </h1>
+                </div>
+              </div>
+              List Goes Here
+            </div>
+        )
     }
 }
 
